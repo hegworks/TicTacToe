acd4748cbaeb29debda7f27f6999b7d3cc403274
HEGworks <hess.gha@gmail.com>
2021-05-20 07:49:31 +0430

    saved history & lifted up sqrVals and handleClick

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,42 +1,17 @@
-import { Typography } from "@material-ui/core";
-import React, { useState } from "react";
+import React from "react";
 import Square from "./Square";
-import { makeStyles } from "@material-ui/core";
 
-const myStyles = makeStyles({
-	myTypography: {
-		whiteSpace: "pre"
-	}
-});
-
-function Board() {
-	// states
-	const [sqrVals, setSqrVals] = useState(Array(9).fill(null));
-	const [xIsNext, setXIsNext] = useState(true);
-
-	let status = "";
-	let winner = calculateWinner(sqrVals);
-	status = winner
-		? winner + " Won!"
-		: (status = xIsNext ? "X\t\t\t<====\t\t\tO" : "X\t\t\t====>\t\t\tO");
-
+function Board(props) {
 	let renderSquare = i => {
-		return <Square value={sqrVals[i]} onClick={() => handleClick(i)} />;
+		return (
+			<Square
+				value={props.sqrVals[i]}
+				onClick={() => props.handleClick(i)}
+			/>
+		);
 	};
-	let handleClick = i => {
-		if (!sqrVals[i] && !winner) {
-			const sqrs = sqrVals.slice();
-			sqrs[i] = xIsNext ? "X" : "O";
-			setXIsNext(!xIsNext);
-			setSqrVals(sqrs);
-		}
-	};
-
 	return (
 		<div>
-			<Typography className={myStyles().myTypography}>
-				{status}
-			</Typography>
 			<div className="board-row">
 				{renderSquare(0)}
 				{renderSquare(1)}
@@ -54,30 +29,6 @@ function Board() {
 			</div>
 		</div>
 	);
-}
-
-function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (
-			squares[a] &&
-			squares[a] === squares[b] &&
-			squares[a] === squares[c]
-		) {
-			return squares[a];
-		}
-	}
-	return null;
 }
 
 export default Board;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,21 +1,85 @@
 import React, { Component } from "react";
 import Board from "./Board";
+import { Typography } from "@material-ui/core";
 
 class Game extends Component {
-	state = {};
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [{ squares: Array(9).fill(null) }],
+			xIsNext: true
+		};
+	}
+
 	render() {
+		const history = this.state.history;
+		const current = history[history.length - 1];
+		const winner = calculateWinner(current.squares);
+
+		let status;
+		status = winner
+			? winner + " Won!"
+			: (status = this.state.xIsNext
+					? "X\t\t\t<====\t\t\tO"
+					: "X\t\t\t====>\t\t\tO");
+
+		let handleClick = i => {
+			const history = this.state.history;
+			const current = history[history.length - 1];
+			const squares = current.squares.slice();
+			if (!squares[i] && !winner) {
+				let sqrs = squares.slice();
+				sqrs[i] = this.state.xIsNext ? "X" : "O";
+				this.setState({
+					history: history.concat([
+						{
+							squares: sqrs
+						}
+					]),
+					xIsNext: !this.state.xIsNext
+				});
+			}
+		};
+
 		return (
 			<div className="game">
+				<Typography style={{ whiteSpace: "pre" }}>{status}</Typography>
 				<div className="game-board">
-					<Board />
+					<Board
+						sqrVals={current.squares}
+						handleClick={i => handleClick(i)}
+					/>
 				</div>
 				<div className="game-info">
-					<div>{/* status*/}</div>
 					<div>{/* TODO */}</div>
 				</div>
 			</div>
 		);
 	}
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
 export default Game;
