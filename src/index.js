a0f922b6bcae0d72cf43c8805d5e03e26d44fd63
HEGworks <hess.gha@gmail.com>
2021-05-20 08:56:07 +0430

    dark theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { createMuiTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
 import ReactDOM from "react-dom";
 import Game from "./components/Game";
 import "./index.css";
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+const darkTheme = createMuiTheme({
+	palette: {
+		type: "dark"
+	}
+});
+
+ReactDOM.render(
+	<ThemeProvider theme={darkTheme}>
+		<CssBaseline />
+		<Game />
+	</ThemeProvider>,
+	document.getElementById("root")
+);
