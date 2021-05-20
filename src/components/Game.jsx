c1ae3a5f2be28d624cd7127e44ad80ca059557fe
HEGworks <hess.gha@gmail.com>
2021-05-20 08:39:25 +0430

    completed game with history selection

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,20 +1,64 @@
 import React, { Component } from "react";
 import Board from "./Board";
-import { Typography } from "@material-ui/core";
+import {
+	List,
+	ListItem,
+	ListItemText,
+	ListSubheader,
+	Typography
+} from "@material-ui/core";
 
 class Game extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			history: [{ squares: Array(9).fill(null) }],
-			xIsNext: true
+			xIsNext: true,
+			stepNumber: 0
 		};
+	}
+
+	handleClick = i => {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		if (!squares[i] && !this.winner) {
+			let sqrs = squares.slice();
+			sqrs[i] = this.state.xIsNext ? "X" : "O";
+			this.setState({
+				history: history.concat([
+					{
+						squares: sqrs
+					}
+				]),
+				stepNumber: history.length,
+				xIsNext: !this.state.xIsNext
+			});
+		}
+	};
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: step % 2 === 0
+		});
 	}
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
+
+		const moves = history.map((step, move) => {
+			const desc = move ? "Go to move #" + move : "Go to game start";
+			return (
+				<ListItem key={move} button>
+					<ListItemText on onClick={() => this.jumpTo(move)}>
+						{desc}
+					</ListItemText>
+				</ListItem>
+			);
+		});
 
 		let status;
 		status = winner
@@ -23,36 +67,18 @@ class Game extends Component {
 					? "X\t\t\t<====\t\t\tO"
 					: "X\t\t\t====>\t\t\tO");
 
-		let handleClick = i => {
-			const history = this.state.history;
-			const current = history[history.length - 1];
-			const squares = current.squares.slice();
-			if (!squares[i] && !winner) {
-				let sqrs = squares.slice();
-				sqrs[i] = this.state.xIsNext ? "X" : "O";
-				this.setState({
-					history: history.concat([
-						{
-							squares: sqrs
-						}
-					]),
-					xIsNext: !this.state.xIsNext
-				});
-			}
-		};
-
 		return (
 			<div className="game">
 				<Typography style={{ whiteSpace: "pre" }}>{status}</Typography>
 				<div className="game-board">
 					<Board
 						sqrVals={current.squares}
-						handleClick={i => handleClick(i)}
+						handleClick={i => this.handleClick(i)}
 					/>
 				</div>
-				<div className="game-info">
-					<div>{/* TODO */}</div>
-				</div>
+				<List subheader={<ListSubheader>History</ListSubheader>}>
+					{moves}
+				</List>
 			</div>
 		);
 	}
